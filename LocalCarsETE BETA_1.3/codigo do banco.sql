994b2bec2ba07a05f2cae29782c16ca70ada0664
CezarDamasceno <juliocezardamascenosousa4@gmail.com>
2025-04-07 10:27:14 -0300

    Delete LocalCarsETE BETA_1.3 directory

--- LocalCarsETE BETA_1.3/codigo do banco.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE locacao_veiculos (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    email VARCHAR(100),
-    datanascimento DATE,
-    telefone VARCHAR(15),
-    estadocivil VARCHAR(20)
-);
